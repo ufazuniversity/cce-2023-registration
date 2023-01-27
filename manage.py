a2745a464bdaabe17170f6b2d79054c0976ea1d1
Gadir Rustamli <g.rustamli@ufaz.az>
2023-01-27 17:19:36 +0400

    Changed folder structure

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deleted.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cce_2023_registration.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
