021a37daba7bca695b9c14072aa7aed1462702ad
Gadir Rustamli <g.rustamli@ufaz.az>
2023-01-27 18:12:46 +0400

    Get ALLOWED_HOSTS from .env or environment var

--- cce_2023_registration/settings.py
+++ cce_2023_registration/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-&!$%afenbq^ehf0^e!r%!gm)!%!r%3p*vu8(a!2q8@ro145h19
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=list, default=[])
 
 
 # Application definition
