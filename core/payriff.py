58074a7680ef336ccd2206ae2dd36fa9b964bb96
Gadir Rustamli <g.rustamli@ufaz.az>
2023-01-30 13:01:45 +0400

    Read settings from env

--- core/payriff.py
+++ core/payriff.py
@@ -1,27 +1,68 @@
 import requests
 import json
-import dataclasses
+import logging
 from django.conf import settings
 
-AUTHORIZATION_HEADER = "Authorization"
+logger = logging.getLogger(__name__)
 
 
-def request_header(secret: str = settings.PAYRIFF_SECRET):
-    return {AUTHORIZATION_HEADER: secret, "Content-type": "application/json"}
+def request_header(secret: str):
+    return {"Authorization": secret, "Content-type": "application/json"}
 
 
 def order_json_payload(
     amount: float,
-    currency: str = settings.ORDER_CURRENCY,
-    description: str = settings.ORDER_DESCRIPTION,
-    merchant_id=settings.PAYRIFF_MERCHANT_ID,
+    currency: str,
+    description: str,
+    merchant_id: str,
+    approveURL: str,
+    cancelURL: str,
+    declineURL: str,
+    language: str,
+) -> str:
+    d = {
+        "body": {
+            "amount": amount,
+            "currencyType": currency,
+            "description": description,
+            "language": language,
+            "approveURL": approveURL,
+            "cancelURL": cancelURL,
+            "declineURL": declineURL,
+        },
+        "merchant": merchant_id,
+    }
+    return json.dumps(d)
+
+
+def create_order(
+    amount: float,
+    currency: str = settings.PAYRIFF_CURRENCY,
+    description: str = settings.PAYRIFF_ORDER_DESCRIPTION,
+    secret: str = settings.PAYRIFF_SECRET,
+    merchant_id: str = settings.PAYRIFF_MERCHANT_ID,
+    timeout: int = settings.PAYRIFF_REQUEST_TIMEOUT,
+    approveURL: str = settings.PAYRIFF_APPROVE_URL,
+    cancelURL: str = settings.PAYRIFF_CANCEL_URL,
+    declineURL: str = settings.PAYRIFF_DECLINE_URL,
+    language: str = settings.PAYRIFF_DEFAULT_LANGUAGE,
 ):
-    pass
-
-
-def post_create_order(amount: float):
-    r = requests.post(
-        settings.PAYRIFF_BASE_URL,
-        headers=request_header(settings.secret),
-        data=order_json_payload(amount),
-    )
+    try:
+        payload = order_json_payload(
+            amount=amount,
+            currency=currency,
+            description=description,
+            merchant_id=merchant_id,
+            approveURL=approveURL,
+            cancelURL=cancelURL,
+            declineURL=declineURL,
+            language=language,
+        )
+        r = requests.post(
+            settings.PAYRIFF_BASE_URL,
+            headers=request_header(secret),
+            data=order_json_payload(amount, currency, description, merchant_id),
+            timeout=timeout,
+        )
+    except requests.exceptions.Timeout:
+        logger.error("Request timed out.")
