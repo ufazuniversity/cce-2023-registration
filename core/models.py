66dcc29cef17390b58dbcfe3b86bf3d1d9e5f20e
Gadir Rustamli <g.rustamli@ufaz.az>
2023-01-31 17:58:11 +0400

    Added is_limited and no_available fields to Ticket model

--- core/models.py
+++ core/models.py
@@ -19,6 +19,8 @@ class Ticket(models.Model):
     variant = models.CharField(
         max_length=20, choices=VARIANT_CHOICES, null=True, blank=True
     )
+    is_limited = models.BooleanField(default=False)
+    no_available = models.PositiveIntegerField("Number of available tickets", null=True, blank=True)
 
     def __str__(self):
         return self.name
