d7007c102ca5d90365819a0e036daadd2e334b3a
Gadir Rustamli <g.rustamli@ufaz.az>
2023-02-08 19:42:54 +0400

    refactor: Set phonenumber widget to PhoneNumberPrefixWidget with custom country choices

--- core/forms.py
+++ core/forms.py
@@ -1,22 +1,40 @@
+import functools
+
 from django import forms
+from django import utils
+from django.conf import settings
+from phonenumber_field import widgets as pn_widgets
 
 from . import models
+
+
+@functools.cache
+def area_code_choices():
+    language = utils.translation.get_language() or settings.LANGUAGE_CODE
+    default_choices = pn_widgets.localized_choices(language)
+    default_choices.sort(key=lambda item: item[1])
+    codes_to_remove = ["AM"]
+    choices = [i for i in default_choices if i[0] not in codes_to_remove]
+    return choices
 
 
 class ParticipantForm(forms.ModelForm):
     allergies = forms.CharField(widget=forms.Textarea, required=False)
     special_request = forms.CharField(widget=forms.Textarea, required=False)
 
-    # phone_number = pn_fields.PhoneNumberField(region="AZ")
-
     class Meta:
         model = models.Participant
         exclude = ["order_ticket"]
+        widgets = {
+            "phone_number": pn_widgets.PhoneNumberPrefixWidget(
+                country_choices=area_code_choices()
+            )
+        }
 
     def __init__(self, includes_meal: bool = True, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for f in self.fields.values():
-            f.widget.attrs.update({'class': 'form-control'})
+            f.widget.attrs.update({"class": "form-control"})
         if not includes_meal:
             del self.fields["allergies"]
             del self.fields["special_request"]
