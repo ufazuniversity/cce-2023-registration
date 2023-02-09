218fc437fc0379bf9ecaac6fe8b45c575a7e4de9
Gadir Rustamli <g.rustamli@ufaz.az>
2023-02-09 20:01:03 +0400

    fix: removed paratheses from the views

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     urls.path("", views.index, name="index"),
     urls.path("tickets/<int:pk>/buy", views.BuyTicketView.as_view(), name="buy-ticket"),
-    urls.path("order-approved", views.order_approved(), name="order-approved"),
-    urls.path("order-declined", views.order_declined(), name="order-declined"),
-    urls.path("order-cancelled", views.order_cancelled(), name="order-cancelled"),
+    urls.path("order-approved", views.order_approved, name="order-approved"),
+    urls.path("order-declined", views.order_declined, name="order-declined"),
+    urls.path("order-cancelled", views.order_cancelled, name="order-cancelled"),
 ]
