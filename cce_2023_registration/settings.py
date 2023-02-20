527bc13c82f48e10d3ad119ddcddf03d4aea2004
Gadir Rustamli <g.rustamli@ufaz.az>
2023-02-20 11:37:03 +0400

    refactor: All only one email address per user

--- cce_2023_registration/settings.py
+++ cce_2023_registration/settings.py
@@ -180,6 +180,7 @@ ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
+ACCOUNT_MAX_EMAIL_ADDRESSES = 1
 ACCOUNT_FORMS = {
     "signup": "core.forms.SignupForm",
     "login": "core.forms.LoginForm",
