b4feef897d9fa4a8413556ca521334497aee68be
Gadir Rustamli <g.rustamli@ufaz.az>
2023-04-10 17:45:09 +0400

    removed print statements

--- cce_2023_registration/settings.py
+++ cce_2023_registration/settings.py
@@ -86,6 +86,7 @@ WSGI_APPLICATION = "cce_2023_registration.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
 DATABASES = {
     "default": dj_database_url.config(
         conn_max_age=600,
