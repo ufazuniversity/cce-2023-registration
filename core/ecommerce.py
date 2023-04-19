34b078ab4ea33d3dafd00651cc7b12a8ed05b149
Gadir Rustamli <g.rustamli@ufaz.az>
2023-04-19 18:23:38 +0400

    updated create_order function

--- core/ecommerce.py
+++ core/ecommerce.py
@@ -1,30 +1,54 @@
 import requests
 from django.conf import settings
 from django.template import loader
+import dataclasses
+from xml.etree import ElementTree as ET
 
-CREATE_ORDER_TEMPLATE_NAME='ecommerce/create_order.xml'
-CREATE_ORDER_HEADERS = {
-    'Content-Type': 'application/xml
-}
+CREATE_ORDER_TEMPLATE_NAME = "ecommerce/create-order.xml"
+CREATE_ORDER_HEADERS = {"Content-Type": "application/xml"}
+
+
+@dataclasses.dataclass
+class CreateOrderPayload:
+    amount: int
+    approve_url: str
+    cancel_url: str
+    decline_url: str
+    description: str
+    merchant_id: str
+    currency: str
+
+    def to_xml(self):
+        return loader.render_to_string(
+            CREATE_ORDER_TEMPLATE_NAME, dataclasses.asdict(self)
+        )
+
+
 def create_order(
     amount,
     approve_url=settings.KB_ECOMM_ORDER_APPROVE_URL,
     cancel_url=settings.KB_ECOMM_ORDER_CANCEL_URL,
     decline_url=settings.KB_ECOMM_ORDER_DECLINE_URL,
-    description=settings.KB_ECOMMERCE_ORDER_DESCRIPTION,
-    url=settings.KB_ECOMMERCE_URL,
-    merchant_id=settings.KB_ECOMMERCE_MERCHANT_ID,
-    currency=settings.KB_ECOMMERCE_CURRENCY,
+    description=settings.KB_ECOMM_ORDER_DESCRIPTION,
+    url=settings.KB_ECOMM_URL,
+    merchant_id=settings.KB_ECOMM_MERCHANT_ID,
+    currency=settings.KB_ECOMM_CURRENCY,
 ):
     """Sending order request to Kapital Ecommerce API"""
-    data = {
-        "amount": amount,
-        "approve_url": approve_url,
-        "cancel_url": cancel_url,
-        "decline_url": decline_url,
-        "description": description,
-        "merchant_id": merchant_id,
-        "currency": currency,
-    }
-    xml_out = loader.render_to_string(CREATE_ORDER_TEMPLATE_NAME, data)
-    response = requests.post(url, data=xml_out, headers=CREATE_ORDER_HEADERS, verify=False)
+    payload = CreateOrderPayload(
+        amount, approve_url, cancel_url, decline_url, description, merchant_id, currency
+    ).to_xml()
+    response = requests.post(
+        url,
+        data=payload,
+        headers=CREATE_ORDER_HEADERS,
+        verify=False,
+        cert=(settings.KB_ECOMM_CERT_PATH, settings.KB_ECOMM_CERT_KEY_PATH),
+    )
+
+    root = ET.fromstring(response.text)
+    order_id = root.find("**/OrderID").text
+    session_id = root.find("**/SessionID").text
+    base_url = root.find("**/URL").text
+    url = f"{base_url}?ORDERID={order_id}&SESSIONID={session_id}"
+    return order_id, session_id, url
