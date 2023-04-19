0009bf839cb403eb1f6facdd4b3b2ce1e711cece
Gadir Rustamli <g.rustamli@ufaz.az>
2023-04-19 14:16:16 +0400

    added __str__ method to Participant

--- core/models.py
+++ core/models.py
@@ -116,6 +116,9 @@ class Participant(models.Model):
     )
     institution = models.CharField("Institution name", max_length=100, null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.title} {self.fullname} <{self.email}>"
+
     @property
     def fields(self):
         return self._meta.get_fields()
