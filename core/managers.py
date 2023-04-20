2430cd0553d105b8d0c70f79bc89675b48e8e4be
Gadir Rustamli <g.rustamli@ufaz.az>
2023-04-20 18:16:36 +0400

    fix: corrected the xml tag paths

--- core/managers.py
+++ core/managers.py
@@ -5,9 +5,9 @@ from xml.etree import ElementTree as ET
 class KBOrderManager(models.Manager):
     def update_from_response_xml(self, payload: str):
         root = ET.fromstring(payload)
-        order_id = root.find("**/OrderID").text
-        session_id = root.find("**/SessionID").text
-        status = root.find("**/OrderStatus").text
+        order_id = root.find("./Message/OrderID").text
+        session_id = root.find("./Message/SessionID").text
+        status = root.find("./Message/OrderStatus").text
         (
             self.get_queryset()
             .filter(order_id=order_id, session_id=session_id)
