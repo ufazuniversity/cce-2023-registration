33800276ed433a5997042493530dbe6fca562f4e
Gadir Rustamli <gadir.rustamli@outlook.com>
2023-04-22 17:10:20 +0400

    completely removed django-browser-reload

--- cce_2023_registration/settings.py
+++ cce_2023_registration/settings.py
@@ -61,7 +61,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "cce_2023_registration.urls"
